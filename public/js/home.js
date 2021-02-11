92e52190ec02af171623099e684ea8b636f7696e
kacihmd <kaci.hmd76@gmail.com>
2021-02-11 15:07:10 +0100

    beautifying

--- public/js/home.js
+++ public/js/home.js
@@ -1,52 +1,53 @@
-window.addEventListener("keydown", function(event) {
+window.addEventListener("keydown", getkeyAndRefreshOutput, true);
+
+function getkeyAndRefreshOutput(event) {
     if (stopped) {
+        chronoReset();
         chronoStart();
     }
-    let str = "KeyboardEvent: key='" + event.key + "' | code='" +
-                event.code + "'  time: " + document.getElementById("chronotime").value;
+    let str = "KeyboardEvent: key='" + event.key + "' | code='" + event.code + 
+        "'  time: " + document.getElementById("chronotime").value;
     let el = document.createElement("span");
     el.innerHTML = str + "<br/>";
-    
     document.getElementById("output").appendChild(el);
-    }, true);
+}
 
 
-
-var start = 0
-var timerID = 0
+var start = 0;
+var timerID = 0;
 var stopped = true;
 
-function chrono(){
-    var end = 0
-    var diff = 0
-	end = new Date()
-	diff = end - start
-	diff = new Date(diff)
-	var msec = diff.getMilliseconds()
-	var sec = diff.getSeconds()
-	var min = diff.getMinutes()
-	var hr = diff.getHours()-1
-	if (min < 10){
-		min = "0" + min
+function chrono() {
+    var end = 0;
+    var diff = 0;
+	end = new Date();
+	diff = end - start;
+	diff = new Date(diff);
+	var msec = diff.getMilliseconds();
+	var sec = diff.getSeconds();
+	var min = diff.getMinutes();
+	var hr = diff.getHours() - 1;
+	if (min < 10) {
+		min = "0" + min;
 	}
-	if (sec < 10){
-		sec = "0" + sec
+	if (sec < 10) {
+		sec = "0" + sec;
 	}
-	if(msec < 10){
-		msec = "00" +msec
+	if (msec < 10) {
+		msec = "00" + msec;
 	}
-	else if(msec < 100){
-		msec = "0" +msec
+	else if (msec < 100) {
+		msec = "0" + msec;
 	}
-	document.getElementById("chronotime").value = hr + ":" + min + ":" + sec + ":" + msec
-	timerID = setTimeout("chrono()", 10)
+	document.getElementById("chronotime").value = hr + ":" + min + ":" + sec + ":" + msec;
+	timerID = setTimeout("chrono()", 10);
 }
-function chronoStart(){
+function chronoStart() {
     stopped = false;
-	document.chronoForm.startstop.value = "stop!"
-	document.chronoForm.startstop.onclick = chronoStop
-	start = new Date()
-	chrono()
+	document.chronoForm.startstop.value = "stop!";
+	document.chronoForm.startstop.onclick = chronoStop;
+	start = new Date();
+	chrono();
 }
 
 function chronoReset() {
@@ -56,7 +57,7 @@ function chronoReset() {
     document.getElementById("output").innerHTML = "";
 }
 
-function chronoStop(){
+function chronoStop() {
     stopped = true;
 	document.chronoForm.startstop.value = "reset!";
 	document.chronoForm.startstop.onclick = chronoReset;

ed00339d56eaffb64ad8ccd080cf30c1bfa65e85
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-02-26 00:49:00 +0100

    Ajoute une persistence de couleur au basculeur de thème

--- public/js/theme.js
+++ public/js/theme.js
@@ -18,7 +18,11 @@ const button = document.getElementById('theme-switcher')
 button.addEventListener('click', () => {
   if (manager.current === 'light') {
     manager.setCurrent('dark')
+    button.classList.remove('red-switcher')
+    button.classList.add('blue-switcher')
   } else {
     manager.setCurrent('light')
+    button.classList.remove('blue-switcher')
+    button.classList.add('red-switcher')
   }
 })

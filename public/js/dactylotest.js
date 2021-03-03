54e68683a8733b4f31e3dc7ef3eba4b72fbc74ee
kacihmd <kaci.hmd76@gmail.com>
2021-03-03 17:01:29 +0100

    Merge logique après réunion

--- public/js/dactylotest.js
+++ public/js/dactylotest.js
@@ -68,6 +68,10 @@ class SpanManager {
     this.parentNode.appendChild(document.createElement('span'))
   }
 
+  getElement () {
+    return this.parentNode
+  }
+
   append (span) {
     this.spans.push(span)
   }
@@ -115,9 +119,11 @@ class SpanManager {
   }
 }
 
+// -------------------------------------------------------------------------- //
+
 class DactyloTestModel {
   constructor (referenceText) {
-    super()
+    //super()
     this.referenceText = referenceText
     this.lastInput = null
   }
@@ -127,34 +133,82 @@ class DactyloTestModel {
   }
 }
 
+// -------------------------------------------------------------------------- //
+
 class Benchmark {
   constructor (referenceText) {
+    this.referenceText = referenceText
+
     this.model = new DactyloTestModel(referenceText)
-    this.hiddenTextArea = //...
-    this.spanManager = new SpanManager()
+    this.textContainer = new SpanManager(document.getElementById('text-container'))
+    this.inputZone = new SpanManager(document.getElementById('virtual-user-input'))
     this.initialize()
   }
 
   initialize () {
-    this.hiddenTextArea.addEventListener('keydown', (e) => {
-      const char = e.key
+    for (let c of this.referenceText) {
+      this.textContainer.insertLast(c)
+    }
 
-      // if (!/Backspace|^.$/.test(c)) {
-      //   return
-      // }
+    // A MODIFIER POUR CONVENIR AU MVC
+    // INITIALISATION DU CURSEUR
+    this.currSpanIndex = 0
+    this.inputZone.insertLast('')
 
-      if (char === 'Backspace') {
-        this.model.deleteLastinput()
-      } else {
-        this.model.setlastInput(char)
-        if (this.model.isLastInputCorrect()) {
-          this.spanManager(char, 'black')
-        } else {
-          this.spanManager(char, 'red')
-        }
+    this.inputZone.getElement().addEventListener('click', () => {
+      this.inputZone.placeCursor(this.currSpanIndex)
+    })
+
+    this.inputZone.getElement().addEventListener('keydown', (e) => {
+      const c = e.key
+
+      if (!/Backspace|^.$/.test(c)) {
+        return
       }
 
-      this.updateCursor() // 
+      // A MODIFIER AFIN DE CORRESPONDRE AU MVC //
+
+      // Une span curseur est définie comme une span sans caractère possèdant la classe curseur
+      if (c === 'Backspace') {
+        // Si la touche 'effacer' est pressée et qu'il y au moin 1 caractère avant le curseur...
+        if (this.inputZone.cursorIndex > 0) {
+          this.currSpanIndex -= 1
+          // ...on retire la span curseur actuel du conteneur...
+          this.inputZone.removeLast()
+          // ...puis on remplace la span d'avant par un span curseur.
+          this.inputZone.setCharAt('', this.currSpanIndex)
+          this.inputZone.moveCursorLeft()
+        }
+      } else {
+        // Sinon on attribut le caractère tapé au span curseur. 
+        this.inputZone.setCharAt(c, this.currSpanIndex)
+        // Et on ajoute une nouvelle span curseur
+        this.inputZone.insertLast('')
+        this.inputZone.moveCursorRight()
+
+        // Si un mauvais caractère à été tapé on colorie la span en rouge
+        if (c != text.charAt(this.currSpanIndex)) {
+          this.inputZone.spans[this.currSpanIndex].setColor('red')
+        } else {
+          this.inputZone.spans[this.currSpanIndex].setColor('white')
+        }
+        this.currSpanIndex += 1
+      }
+
+      
+      // CA C'EST CE QUE REMI ET THOMAS ONT ECRIT, A FARE EN SORTE QUE CA MARCHE 
+
+      // if (char === 'Backspace') {
+      //   this.model.deleteLastinput()
+      // } else {
+      //   this.model.setlastInput(char)
+      //   if (this.model.isLastInputCorrect()) {
+      //     this.spanManager(c, 'black')
+      //   } else {
+      //     this.spanManager(c, 'red')
+      //   }
+      // }
+      // this.updateCursor() // 
     })
   }
 
@@ -162,6 +216,8 @@ class Benchmark {
     this.spanManager.setCursor(this.model.getIndex())
   }
 }
+
+// -------------------------------------------------------------------------- //
 
 const text = 'La philosophie est une démarche de réflexion critique et de questionnement sur le monde, la connaissance et l\'existence humaine. Elle existe depuis l\'Antiquité en Occident et en Orient, à travers la figure du philosophe, non seulement en tant qu\'activité rationnelle mais aussi comme mode de vie. L\'histoire de la philosophie permet d\'appréhender son évolution.'
 const benchmark = new Benchmark(text)

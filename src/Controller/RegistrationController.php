9fc9c191900963d81d0cbd794845024aab87b0f6
kacihmd <kaci.hmd76@gmail.com>
2021-03-16 19:50:31 +0100

    Connexion après création de compte

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+use Symfony\Component\EventDispatcher\EventDispatcher,
+    Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken,
+    Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
 class RegistrationController extends AbstractController
 {
     /**
@@ -17,6 +21,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -33,9 +41,18 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_login');
+            // Connexion après création du compte
+            // Credit : https://stackoverflow.com/questions/9550079/how-to-programmatically-login-authenticate-a-user
+            $token = new UsernamePasswordToken($user, $form->get('plainPassword')->getData(), "main", $user->getRoles());
+            // For older versions of Symfony, use security.context here
+            $this->get("security.token_storage")->setToken($token);
+            // Fire the login event
+            // Logging the user in above the way we do it doesn't do this automatically
+            $event = new InteractiveLoginEvent($request, $token);
+            (new EventDispatcher())->dispatch($event, "security.interactive_login");
+
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('registration/register.html.twig', [

120d9233fb60b495daa410678717eba48c964dd4
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-03-17 14:20:06 +0100

    Corrige le nom de la table des exercices

--- migrations/Version20210315185026.php
+++ migrations/Version20210315185026.php
@@ -14,18 +14,18 @@ final class Version20210315185026 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Créé la table `exercice`';
+        return 'Créé la table `exercise`';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE exercice (id INT AUTO_INCREMENT NOT NULL, tag VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE exercise (id INT AUTO_INCREMENT NOT NULL, tag VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE exercice');
+        $this->addSql('DROP TABLE exercise');
     }
 }

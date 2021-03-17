6c7b6ceccc88c4e43814926942902c4a368b7ebb
Remi-labbe <labbe.remi@free.fr>
2021-03-17 14:37:25 +0100

    correction migration exercise

--- migrations/Version20210315185026.php
+++ migrations/Version20210315185026.php
@@ -20,7 +20,7 @@ final class Version20210315185026 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE exercise (id INT AUTO_INCREMENT NOT NULL, tag VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE exercise (id INT AUTO_INCREMENT NOT NULL, tag VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void

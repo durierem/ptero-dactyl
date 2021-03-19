49ec9c85188fe0fab434970cb9edf28235fce88c
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-03-19 22:20:20 +0100

    Ajoute l'API de contrôle de session de dactylotest

--- src/Controller/PrivateApiController.php
+++ src/Controller/PrivateApiController.php
@@ -9,10 +9,45 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PrivateApiController extends AbstractController
 {
+    const BENCHMARK = 'benchmark';
+    const EXERCISE = 'exercise';
+    const END = 'save_dactylotest';
+
+    private $sequence = [
+        self::BENCHMARK,
+        self::EXERCISE,
+        self::EXERCISE,
+        self::BENCHMARK,
+        self::EXERCISE,
+        self::EXERCISE,
+        self::BENCHMARK,
+        self::END
+    ];
+
+    /**
+     * @Route("/dactylotest/session")
+     */
+    public function session(SessionInterface $session)
+    {
+        $currentStep = $session->get('step', -1);
+
+        $nextStep = $currentStep + 1;
+
+        if ($nextStep >= count($this->sequence)) {
+            $nextStep = 0;
+        }
+
+        $session->set('step', $nextStep);
+
+        $nextRoute = $this->sequence[$nextStep];
+        return $this->redirectToRoute($nextRoute);
+    }
+
     /**
      * @Route("/dactylotest/save", name="save_dactylotest")
      */

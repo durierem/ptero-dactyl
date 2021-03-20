f30907e0317fcd3a874f11eb2e28d863b72e6686
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-03-21 00:12:50 +0100

    Corrige un mauvais nom de varibale

--- public/js/dactylotest/benchmark.js
+++ public/js/dactylotest/benchmark.js
@@ -129,7 +129,7 @@ class Benchmark {
         this.isInputAllowed = true
       } else {
         this.inputZone.removeCursor()
-        this.isInputInhibited = false
+        this.isInputAllowed = false
         this.data.stopTimer()
       }
     })

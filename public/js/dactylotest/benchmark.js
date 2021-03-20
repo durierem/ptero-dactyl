519a50f056c8095c06b9b176e059517350c3af33
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-03-20 13:27:43 +0100

    Ajoute une gestion du focus sur la zone d'input

--- public/js/dactylotest/benchmark.js
+++ public/js/dactylotest/benchmark.js
@@ -11,6 +11,9 @@ class Benchmark {
     this.textContainer = new SpanManager(document.getElementById('text-container'))
     this.inputZone = new SpanManager(document.getElementById('virtual-user-input'))
     this.firstInput = true
+    this.isFocused = false
+    this.isMouseOver = false
+    this.isInputInhibited = true
     this.lastChar = ' '
     this.initialize()
   }
@@ -21,10 +24,7 @@ class Benchmark {
       this.textContainer.insertLast(c)
     }
 
-    this.inputZone.getElement().addEventListener('click', () => {
-      this.inputZone.insertLast('') // Première span...
-      this.inputZone.placeCursor(0) // ... qui sert de curseur
-    })
+    this.handleFocus()
 
     this.inputZone.getElement().addEventListener('keydown', (e) => {
       if (this.firstInput) {
@@ -55,10 +55,10 @@ class Benchmark {
           this.data.addMistake(this.model.getCurrWord())
         } else {
           const isEndOfWord = !/\d|\w/.test(this.model.getReferenceText()
-                        .charAt(currentUserText().length))
+            .charAt(currentUserText().length))
           console.log(isEndOfWord,
-                      this.model.getReferenceText()
-                        .charAt(currentUserText().length))
+            this.model.getReferenceText()
+              .charAt(currentUserText().length))
           if (isEndOfWord) {
             this.data.resetMis()
             this.data.addWordTime()
@@ -87,18 +87,20 @@ class Benchmark {
           const data = this.data.getData()
           sessionStorage.setItem('bench', JSON.stringify(data))
           location.assign('/dactylotest/exercise')
-        // 2e benchmark on ajoute des donnees
+          // 2e benchmark on ajoute des donnees
         } else if (sessionStorage.getItem('order') === 1) {
           sessionStorage.setItem('order', 2)
           const prevData = JSON.parse(sessionStorage.getItem('bench'))
-          const jason = {bench1: prevData,
-                         bench2: this.data.getData()}
+          const jason = {
+            bench1: prevData,
+            bench2: this.data.getData()
+          }
           sessionStorage.setItem('bench', JSON.stringify(jason))
           location.assign('/dactylotest/exercise')
-        // 3e benchmark on ajoute les dernieres donnees et on envoie en bdd
+          // 3e benchmark on ajoute les dernieres donnees et on envoie en bdd
         } else {
           sessionStorage.removeItem('order')
-          let jason = JSON.parse(sessionStorage.getItem('bench'))
+          const jason = JSON.parse(sessionStorage.getItem('bench'))
           jason.bench3 = this.data.getData()
           const target = '/dactylotest/save'
           $.post(target, { data: JSON.stringify(jason) })
@@ -109,25 +111,46 @@ class Benchmark {
       }
     })
   }
+
+  handleFocus () {
+    this.inputZone.getElement().addEventListener('mouseenter', () => {
+      this.isMouseOver = true
+    })
+    this.inputZone.getElement().addEventListener('mouseleave', () => {
+      this.isMouseOver = false
+    })
+
+    document.body.addEventListener('click', () => {
+      if (this.isMouseOver) {
+        if (this.firstInput) {
+          this.inputZone.insertLast('')
+        }
+        this.inputZone.placeCursor(this.model.getUserText().length)
+      } else {
+        this.inputZone.removeCursor()
+        this.isInputInhibited = false
+      }
+    })
+  }
 }
 
 const defaultText = 'Put all speaking, her69 speaking delicate recurred possible.'
 let benchmark = null
 $(document).ready(() => {
-  let target = '/text/random'
+  const target = '/text/random'
   let prevIds = JSON.parse(sessionStorage.getItem('prev'))
   $.post(target, { bDone: JSON.stringify(prevIds) })
     .done((data) => {
       if (prevIds === null) {
-        prevIds = {b1: data.id}
+        prevIds = { b1: data.id }
         sessionStorage.setItem('prev', JSON.stringify(prevIds))
-      } else if (prevIds.b2 === null){
+      } else if (prevIds.b2 === null) {
         prevIds.b2 = data.id
         sessionStorage.setItem('prev', JSON.stringify(prevIds))
       } else {
         sessionStorage.removeItem('prev')
       }
-      //benchmark = new Benchmark(data.text)
+      // benchmark = new Benchmark(data.text)
       benchmark = new Benchmark(defaultText)
     })
     .fail(() => {

432dcd3829ff29532fe970600bd707faf025eb8d
kacihmd <kaci.hmd76@gmail.com>
2021-03-20 14:18:09 +0100

    Clique sur la fenetre pour fermer le nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,9 +1,15 @@
 function openNav() {
   document.getElementById("nav").style.width = '250px'
   document.body.style.marginLeft = '250px'
+  setTimeout(function() {
+    document.addEventListener('click', closeNav)
+  }, 500)
 }
 
 function closeNav() {
   document.getElementById("nav").style.width = '0'
   document.body.style.marginLeft = '0'
+  setTimeout(function() {
+    document.removeEventListener('click', closeNav)
+  }, 500)
 }

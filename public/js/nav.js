2aa9a476f94ddddbba72fabcdf6ca311adb9f96e
Rémi Durieu <remi.durieu@univ-rouen.fr>
2021-03-20 12:37:12 +0100

    Retravaille le menu de navigation + ajustements

---
+++ public/js/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("nav").style.width = '250px'
+  document.body.style.marginLeft = '250px'
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = '0'
+  document.body.style.marginLeft = '0'
+}
